b06cde90d6e1073d4c205537e3991482f3cbba8e
Daksh Gupta <acedaksh07@gmail.com>
2025-02-28 14:25:54 -0500

    feat: bezier handles & animations

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,6 @@
-use egui::{pos2, Color32, Stroke, Vec2};
+use egui::{lerp, pos2, Color32, Context, Pos2, Stroke, Ui, Vec2};
 use egui_extras::RetainedImage;
+use uuid::Uuid;
 
 // Uncomment this section to get access to the console_log macro
 // Use console_log to print things to console. println macro doesn't work
@@ -46,6 +47,13 @@ pub struct BezPoint {
     pub cp1y: f32,
     pub cp2x: f32,
     pub cp2y: f32,
+
+    // ID
+    id: Uuid,
+    // Hover states
+    p_hov: bool,
+    cp1_hov: bool,
+    cp2_hov: bool,
 }
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
@@ -75,7 +83,100 @@ impl BezPoint {
             cp1y,
             cp2x,
             cp2y,
+            id: Uuid::new_v4(),
+            p_hov: false,
+            cp1_hov: false,
+            cp2_hov: false,
         }
+    }
+    fn draw(
+        &mut self,
+        ui: &mut Ui,
+        ctx: &Context,
+        ratio: f32,
+        origin: Pos2,
+        hover_pos: Option<Pos2>,
+    ) {
+        let r = 5.0; // point radius
+        let r_hov = 8.0; // hover radius
+
+        // Generate an id to keep track of point animations
+        let id = ui.make_persistent_id(self.id);
+        let p_id = id.with(0);
+        let cp1_id = id.with(1);
+        let cp2_id = id.with(2);
+
+        // Main point
+        let x = self.x * ratio + origin.x;
+        let y = self.y * ratio + origin.y;
+        // Control points
+        let cp1x = self.cp1x * ratio + origin.x;
+        let cp1y = self.cp1y * ratio + origin.y;
+        let cp2x = self.cp2x * ratio + origin.x;
+        let cp2y = self.cp2y * ratio + origin.y;
+
+        // Update hover states
+        self.p_hov = false;
+        self.cp1_hov = false;
+        self.cp2_hov = false;
+        if let Some(hover_pos) = hover_pos {
+            let mut distances = [
+                (&mut self.cp1_hov, hover_pos.distance_sq(pos2(cp1x, cp1y))),
+                (&mut self.cp2_hov, hover_pos.distance_sq(pos2(cp2x, cp2y))),
+            ];
+            let mut point_dis = (&mut self.p_hov, hover_pos.distance_sq(pos2(x, y)));
+            let (hovered, min_distance) =
+                distances
+                    .iter_mut()
+                    .fold(&mut point_dis, |min, x| if x.1 < min.1 { x } else { min });
+            if *min_distance < r * r {
+                console_log!("Hovered!");
+                **hovered = true;
+                console_log!(
+                    "POINT: {}\nCONTROL POINT 1: {}\nCONTROL POINT 2: {}",
+                    self.p_hov,
+                    self.cp1_hov,
+                    self.cp2_hov
+                );
+            } else {
+                **hovered = false;
+            }
+        }
+
+        // Update point radii based on hover state
+        let p_r = lerp(r..=r_hov, ctx.animate_bool(p_id, self.p_hov));
+        let cp1_r = lerp(r..=r_hov, ctx.animate_bool(cp1_id, self.cp1_hov));
+        let cp2_r = lerp(r..=r_hov, ctx.animate_bool(cp2_id, self.cp2_hov));
+
+        // Offsets to prevent lines draw inside hollow control points
+        // Essentially just sine and cosine of the angle
+        let dx1 = cp1x - x;
+        let dy1 = cp1y - y;
+        let dx2 = cp2x - x;
+        let dy2 = cp2y - y;
+        let mag1 = (dx1 * dx1 + dy1 * dy1).sqrt();
+        let mag2 = (dx2 * dx2 + dy2 * dy2).sqrt();
+        let xoffset1 = (cp1_r + 1.0) * dx1 / mag1;
+        let yoffset1 = (cp1_r + 1.0) * dy1 / mag1;
+        let xoffset2 = (cp2_r + 1.0) * dx2 / mag2;
+        let yoffset2 = (cp2_r + 1.0) * dy2 / mag2;
+
+        // Control lines
+        ui.painter().line_segment(
+            [pos2(x, y), pos2(cp1x - xoffset1, cp1y - yoffset1)],
+            Stroke::new(2.0, Color32::YELLOW),
+        );
+        ui.painter().line_segment(
+            [pos2(x, y), pos2(cp2x - xoffset2, cp2y - yoffset2)],
+            Stroke::new(2.0, Color32::YELLOW),
+        );
+
+        // Draw points
+        ui.painter().circle_filled(pos2(x, y), p_r, Color32::YELLOW);
+        ui.painter()
+            .circle_stroke(pos2(cp1x, cp1y), cp1_r, Stroke::new(2.0, Color32::YELLOW));
+        ui.painter()
+            .circle_stroke(pos2(cp2x, cp2y), cp2_r, Stroke::new(2.0, Color32::YELLOW));
     }
 }
 
@@ -232,6 +333,17 @@ impl eframe::App for PathyApp {
                 _ => {}
             }
 
+            /* POINT RENDERING */
+            for point in &mut self.points {
+                point.draw(
+                    ui,
+                    ctx,
+                    self.scale as f32 / self.size,
+                    rect.min,
+                    resp.hover_pos(),
+                );
+            }
+
             /* CLICK HANDLERS */
             if resp.clicked() {
                 match &self.cursor_mode {
@@ -249,7 +361,8 @@ impl eframe::App for PathyApp {
                             let x = (pos.x - rect.min.x) * (self.size / self.scale as f32);
                             let y = (pos.y - rect.min.y) * (self.size / self.scale as f32);
                             console_log!("({}, {})", x, y);
-                            self.points.push(BezPoint::new(x, y, 0.0, 0.0, 0.0, 0.0));
+                            self.points
+                                .push(BezPoint::new(x, y, x + 20.0, y + 10.0, x, y + 20.0));
                         }
                     }
                     CursorMode::Edit => {
@@ -260,14 +373,6 @@ impl eframe::App for PathyApp {
                     }
                     _ => {}
                 }
-            }
-
-            /* POINT RENDERING */
-            for point in &self.points {
-                // Calculate points relative to screen
-                let x = point.x * (self.scale as f32 / self.size) + rect.min.x;
-                let y = point.y * (self.scale as f32 / self.size) + rect.min.y;
-                ui.painter().circle_filled(pos2(x, y), 5.0, Color32::YELLOW);
             }
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
